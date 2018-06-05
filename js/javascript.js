d59e889f0451c5ba440f868dd2d12858012b4b18
joonij <37970610+joonij@users.noreply.github.com>
2018-06-05 17:21:09 +0900

    release 20180605

--- js/javascript.js
+++ js/javascript.js
@@ -28,32 +28,34 @@ $(document).ready(function(){
     
     
     //    music play
-    var audio;
+    var music;
     function audio_list() {
         if ($("#cd_list > div").eq(0).hasClass("play")){
-            audio = new Audio("audio/Troye Sivan - Happy Little Pill.mp3");
+            music = document.getElementById("cd_1_music"); 
         } else if ($("#cd_list > div").eq(1).hasClass("play")) {
-            audio = new Audio("audio/Troye Sivan - for him..mp3");
+            music = document.getElementById("cd_2_music"); 
         } else if ($("#cd_list > div").eq(2).hasClass("play")) {
-            audio = new Audio("audio/Troye Sivan - YOUTH.mp3");
+            music = document.getElementById("cd_3_music"); 
         } else if ($("#cd_list > div").eq(3).hasClass("play")) {
-            audio = new Audio("audio/Troye Sivan - Fools.mp3");
+            music = document.getElementById("cd_4_music"); 
         } else if ($("#cd_list > div").eq(4).hasClass("play")) {
-            audio = new Audio("audio/Troye Sivan - WILD.mp3");
+            music = document.getElementById("cd_5_music"); 
         }
     }
+    
+    
     $("#stop_btn").css("display","none");
     $("#play_btn").click(Event, function(){
-        audio_list();
+        audio_list()
         $(this).css("display","none");
         $("#stop_btn").css("display","block");
         $(".play .cd_img").addClass("turn");
-        audio.play();
+        music.play();
         $("#stop_btn").click(Event, function(){
             $(this).css("display","none");
             $("#play_btn").css("display","block");
             $(".play .cd_img").removeClass("turn");
-            audio.pause();
+            music.pause();
         });
     });
     
@@ -62,74 +64,77 @@ $(document).ready(function(){
     $("#cd_list > div").eq(0).addClass("play");
     $("#cd_list > div").eq(1).addClass("next");
     $("#cd_list > div").eq(4).addClass("prev");
-/*    for(var i=0; i<5 ;i++){
-       if( $("#cd_list > div").eq(i).hasClass("play")){
-           var j=4 0 1 2 3
-           $("#cd_list > div").eq(i+4).removeClass("prev");
-           $("#cd_list > div").eq(i).attr("class", "prev");
-           $("#cd_list > div").eq(i+1).attr("class", "play");
-           $("#cd_list > div").eq(i+2).attr("class", "next");
-       }
-    }*/
+
     $("#next_btn").click(Event, function(){
-        audio_list();
-        $("#stop_btn").css("display","none");
-        $("#play_btn").css("display","block");
-        $(".play .cd_img").removeClass("turn");
-        audio.pause();
+            $("#stop_btn").css("display","none");
+            $("#play_btn").css("display","block");
+            $(".play .cd_img").removeClass("turn");
         if ($("#cd_list > div").eq(0).hasClass("play")){
             $("#cd_list > div").eq(4).removeClass("prev");
             $("#cd_list > div").eq(0).attr("class", "prev");
             $("#cd_list > div").eq(1).attr("class", "play");
             $("#cd_list > div").eq(2).attr("class", "next");
+            music.pause();
         } else if ($("#cd_list > div").eq(1).hasClass("play")) {
             $("#cd_list > div").eq(0).removeClass("prev");
             $("#cd_list > div").eq(1).attr("class", "prev");
             $("#cd_list > div").eq(2).attr("class", "play");
             $("#cd_list > div").eq(3).attr("class", "next");
+            music.pause();
         } else if ($("#cd_list > div").eq(2).hasClass("play")) {
             $("#cd_list > div").eq(1).removeClass("prev");
             $("#cd_list > div").eq(2).attr("class", "prev");
             $("#cd_list > div").eq(3).attr("class", "play");
             $("#cd_list > div").eq(4).attr("class", "next");
+            music.pause();
         } else if ($("#cd_list > div").eq(3).hasClass("play")) {
             $("#cd_list > div").eq(2).removeClass("prev");
             $("#cd_list > div").eq(3).attr("class", "prev");
             $("#cd_list > div").eq(4).attr("class", "play");
             $("#cd_list > div").eq(0).attr("class", "next");
+            music.pause();
         } else if ($("#cd_list > div").eq(4).hasClass("play")) {
             $("#cd_list > div").eq(3).removeClass("prev");
             $("#cd_list > div").eq(4).attr("class", "prev");
             $("#cd_list > div").eq(0).attr("class", "play");
             $("#cd_list > div").eq(1).attr("class", "next");
+            music.pause();
         }
     });
     $("#prev_btn").click(Event, function(){
+            $("#stop_btn").css("display","none");
+            $("#play_btn").css("display","block");
+            $(".play .cd_img").removeClass("turn");
         if ($("#cd_list > div").eq(0).hasClass("play")){
             $("#cd_list > div").eq(1).removeClass("next");
             $("#cd_list > div").eq(0).attr("class", "next");
             $("#cd_list > div").eq(4).attr("class", "play");
             $("#cd_list > div").eq(3).attr("class", "prev");
+            music.pause();
         } else if ($("#cd_list > div").eq(4).hasClass("play")) {
             $("#cd_list > div").eq(0).removeClass("next");
             $("#cd_list > div").eq(4).attr("class", "next");
             $("#cd_list > div").eq(3).attr("class", "play");
             $("#cd_list > div").eq(2).attr("class", "prev");
+            music.pause();
         } else if ($("#cd_list > div").eq(3).hasClass("play")) {
             $("#cd_list > div").eq(4).removeClass("next");
             $("#cd_list > div").eq(3).attr("class", "next");
             $("#cd_list > div").eq(2).attr("class", "play");
             $("#cd_list > div").eq(1).attr("class", "prev");
+            music.pause();
         } else if ($("#cd_list > div").eq(2).hasClass("play")) {
             $("#cd_list > div").eq(3).removeClass("next");
             $("#cd_list > div").eq(2).attr("class", "next");
             $("#cd_list > div").eq(1).attr("class", "play");
             $("#cd_list > div").eq(0).attr("class", "prev");
+            music.pause();
         } else if ($("#cd_list > div").eq(1).hasClass("play")) {
             $("#cd_list > div").eq(2).removeClass("next");
             $("#cd_list > div").eq(1).attr("class", "next");
             $("#cd_list > div").eq(0).attr("class", "play");
             $("#cd_list > div").eq(4).attr("class", "prev");
+            music.pause();
         }
     });
     
